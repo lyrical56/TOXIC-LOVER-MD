8809bded08bf854ad4d8bad2a060e6112bcbd480
lyrical56 <sadigom45@gmail.com>
2025-07-02 23:03:17 +0300

    Update set.js

--- set.js
+++ set.js
@@ -10,8 +10,8 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
 module.exports = { session: process.env.SESSION_ID || 'ezra',
     PREFIXE: process.env.PREFIX || "+",
     GITHUB : process.env.GITHUB|| 'https://github.com/toxiclover-tech/TOXIC-LOVER-MD',
-    OWNER_NAME : process.env.OWNER_NAME || "toxic lover",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "254717263689",  
+    OWNER_NAME : process.env.OWNER_NAME || "salo",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "254702689122",  
               
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
